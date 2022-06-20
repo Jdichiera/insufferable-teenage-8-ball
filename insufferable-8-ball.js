f5a1fcbcd6c18d83b9e87f7f0434385e9adfcdb5
Jeramy Dichiera <jeramy.dichiera@ncino.com>
2022-06-20 13:57:37 -0400

    Creating functions

--- insufferable-8-ball.js
+++ insufferable-8-ball.js
@@ -3,11 +3,33 @@ import { middleMessagePartials } from "./message_partials/mid-message-partials.j
 import { endMessagePartials } from "./message_partials/end-message-partials.js";
 
 const generateMessage = () => {
-  return (
-    beginningMessagePartials[0] +
-    middleMessagePartials[0] +
-    endMessagePartials[0]
-  );
+  let messageBeginning = selectMessageBeginning();
+  let messageMiddle = selectMessageMiddle();
+  let messageEnding = selectMessageEnding();
+
+  return formatMesssage(messageBeginning, messageMiddle, messageEnding);
 };
 
-console.log(`--== ${generateMessage()} ==--`);
+const formatMesssage = (message1, message2, message3) => {
+  return `${message1} ${message2} ${message3}`;
+};
+
+const printMessage = () => {
+  let message = generateMessage();
+
+  console.log(`--== ${message} ==--`);
+};
+
+const selectMessageBeginning = () => {
+  return beginningMessagePartials[0];
+};
+
+const selectMessageMiddle = () => {
+  return middleMessagePartials[0];
+};
+
+const selectMessageEnding = () => {
+  return endMessagePartials[0];
+};
+
+printMessage();
