1758c1b87c0b9011dc889b400446a98e35dca433
Jeramy Dichiera <jeramy.dichiera@ncino.com>
2022-06-20 13:50:37 -0400

    Start breaking file into functions

--- insufferable-8-ball.js
+++ insufferable-8-ball.js
@@ -2,8 +2,12 @@ import { beginningMessagePartials } from "./message_partials/beg-message-partial
 import { middleMessagePartials } from "./message_partials/mid-message-partials.js";
 import { endMessagePartials } from "./message_partials/end-message-partials.js";
 
-let fullMessage =
-  beginningMessagePartials[0] +
-  middleMessagePartials[0] +
-  endMessagePartials[0];
-console.log(`--== ${fullMessage} ==--`);
+const generateMessage = () => {
+  return (
+    beginningMessagePartials[0] +
+    middleMessagePartials[0] +
+    endMessagePartials[0]
+  );
+};
+
+console.log(`--== ${generateMessage()} ==--`);
