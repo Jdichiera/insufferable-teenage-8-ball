3cd769dbb6bfea97ddea03c6afd954973af071e3
Jeramy Dichiera <jeramy.dichiera@ncino.com>
2022-06-20 14:44:32 -0400

    Add greeting message and firmat return

--- insufferable-8-ball.js
+++ insufferable-8-ball.js
@@ -1,13 +1,25 @@
+import { greetingMessagePartials } from "./message_partials/greeting-message-partials.js";
 import { beginningMessagePartials } from "./message_partials/beg-message-partials.js";
 import { middleMessagePartials } from "./message_partials/mid-message-partials.js";
 import { endMessagePartials } from "./message_partials/end-message-partials.js";
 
 const generateMessage = () => {
+  let greeting = selectGreeting();
   let messageBeginning = selectMessageBeginning();
   let messageMiddle = selectMessageMiddle();
   let messageEnding = selectMessageEnding();
 
-  return formatMesssage(messageBeginning, messageMiddle, messageEnding);
+  return formatMesssage(
+    greeting,
+    messageBeginning,
+    messageMiddle,
+    messageEnding
+  );
+};
+
+const selectGreeting = () => {
+  let randomIndex = getRandomIndex(greetingMessagePartials.length);
+  return greetingMessagePartials[randomIndex];
 };
 
 const selectMessageBeginning = () => {
@@ -25,8 +37,16 @@ const selectMessageEnding = () => {
   return endMessagePartials[randomIndex];
 };
 
-const formatMesssage = (message1, message2, message3) => {
-  return `${message1} ${message2} ${message3}`;
+const formatMesssage = (greeting, message1, message2, message3) => {
+  let message =
+    greeting +
+    "\n ... *considers your question* ... \n " +
+    message1 +
+    "\n" +
+    message2 +
+    "\n" +
+    message3;
+  return message;
 };
 
 const getRandomIndex = (length) => {
@@ -36,7 +56,7 @@ const getRandomIndex = (length) => {
 const printMessage = () => {
   let message = generateMessage();
 
-  console.log(`--== ${message} ==--`);
+  console.log(message);
 };
 
 printMessage();
---
+++ message_partials/greeting-message-partials.js
@@ -0,0 +1,6 @@
+export let greetingMessagePartials = [
+  "What now",
+  "Im busy ... geez",
+  "Ugh cant I ever just relax",
+  "What is it with you people",
+];
